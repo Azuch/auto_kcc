2cf1ff3212b895636775769c68ada4fb29d75a8d
Azuch <50861752+Azuch@users.noreply.github.com>
2024-04-27 13:47:55 +0700

    Create init.sh
    just a test

---
+++ init.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/ciromattia/kcc.git 
+cd kcc
+apt install python3-venv -y
+python3 -m venv venv
+source ./venv/bin/activate
+sudo apt-get install python3 python3-dev python3-pip libpng-dev libjpeg-dev p7zip-full python3-pyqt5 -y
+pip3 install -r 'requirements.txt' 
+cp -R kindlegen '/usr/local/bin'
+chmod +rwx '/usr/local/bin/kindlegen' 
