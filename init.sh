4f50843af0ccfcc5aa58cf07b0ff6f46965c2ecb
Azuch <50861752+Azuch@users.noreply.github.com>
2024-04-27 14:01:11 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ python3 -m venv venv
 source ./venv/bin/activate
 sudo apt-get install python3 python3-dev python3-pip libpng-dev libjpeg-dev p7zip-full python3-pyqt5 -y
 pip3 install -r 'requirements.txt' 
-cp -R ../kindlegen '/usr/local/bin'
+tar -xvf ../kindlegen.tar.gz
+cp -R ./kindlegen '/usr/local/bin'
 chmod +rwx '/usr/local/bin/kindlegen' 
